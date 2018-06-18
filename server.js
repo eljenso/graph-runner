fa52f67b222f9260090dcff6f2edcb292168b445
Jens Böttcher <eljenso.boettcher@gmail.com>
2018-06-18 19:03:23 +0200

    Switch to standalone apollo-server (v2)

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-const Hapi = require("hapi");
-const { graphqlHapi, graphiqlHapi } = require("apollo-server-hapi");
-const { makeExecutableSchema } = require("graphql-tools");
+const { ApolloServer, gql } = require("apollo-server");
 
 const { typeDefs } = require("./types");
 const { resolvers } = require("./resolvers");
@@ -8,47 +6,8 @@ const { resolvers } = require("./resolvers");
 const HOST = "0.0.0.0";
 const PORT = 3000;
 
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen(PORT, HOST).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
 });
-
-async function StartServer() {
-  const server = new Hapi.server({
-    host: HOST,
-    port: PORT
-  });
-
-  await server.register({
-    plugin: graphqlHapi,
-    options: {
-      path: "/graphql",
-      graphqlOptions: {
-        schema
-      },
-      route: {
-        cors: true
-      }
-    }
-  });
-
-  await server.register({
-    plugin: graphiqlHapi,
-    options: {
-      path: "/graphiql",
-      graphiqlOptions: {
-        endpointURL: "/graphql"
-      }
-    }
-  });
-
-  try {
-    await server.start();
-  } catch (err) {
-    console.log(`Error while starting server: ${err.message}`);
-  }
-
-  console.log(`Server running at: ${server.info.uri}`);
-}
-
-StartServer();
