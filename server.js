6e85647ec7767fc847206cab66fd2dd9b9284f3f
Jens Böttcher <eljenso.boettcher@gmail.com>
2018-06-29 11:36:55 +0200

    Enable tracing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { resolvers } = require("./resolvers");
 const HOST = "0.0.0.0";
 const PORT = 3000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ tracing: true, typeDefs, resolvers });
 
 server.listen(PORT, HOST).then(({ url }) => {
   console.log(`Server ready at ${url}`);
